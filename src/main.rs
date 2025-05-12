d637fe8940793cd28c92ea7d59b4124481db89a7
Nils Martel <nilsmartel@yahoo.de>
2025-05-12 09:47:37 +0200

    set up basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use rustpython_parser::{self, Mode};
+use std::any::Any;
+
+use itertools::join;
 use rustpython_parser::ast;
+use rustpython_parser::{self, Mode};
 
 fn main() {
+    let filepath = std::env::args().nth(1).expect("filepath as first argument");
     let source_path = ".";
-    let file = "some_path";
-    let source = std::fs::read_to_string(file).expect("read file");
-
-    let ast = rustpython_parser::parse(&source, Mode::Module, source_path);
+    let ast = read_ast(source_path, &filepath);
     let ast = ast.expect("valid python programm");
 
     let module = match ast {
@@ -14,77 +15,141 @@ fn main() {
         _ => panic!("expected module"),
     };
 
-    for statement in module.body {
-        match statement {
-            ast::Stmt::FunctionDef(f) => s_functiondef(f),
-            ast::Stmt::AsyncFunctionDef(_) => todo!(),
-            ast::Stmt::ClassDef(_) => todo!(),
-            ast::Stmt::Return(_) => todo!(),
-            ast::Stmt::Delete(_) => todo!(),
-            ast::Stmt::Assign(_) => todo!(),
-            ast::Stmt::TypeAlias(_) => todo!(),
-            ast::Stmt::AugAssign(_) => todo!(),
-            ast::Stmt::AnnAssign(_) => todo!(),
-            ast::Stmt::For(_) => todo!(),
-            ast::Stmt::AsyncFor(_) => todo!(),
-            ast::Stmt::While(_) => todo!(),
-            ast::Stmt::If(_) => todo!(),
-            ast::Stmt::With(_) => todo!(),
-            ast::Stmt::AsyncWith(_) => todo!(),
-            ast::Stmt::Match(_) => todo!(),
-            ast::Stmt::Raise(_) => todo!(),
-            ast::Stmt::Try(_) => todo!(),
-            ast::Stmt::TryStar(_) => todo!(),
-            ast::Stmt::Assert(_) => todo!(),
-            ast::Stmt::Import(_) => todo!(),
-            ast::Stmt::ImportFrom(_) => todo!(),
-            ast::Stmt::Global(_) => todo!(),
-            ast::Stmt::Nonlocal(_) => todo!(),
-            ast::Stmt::Expr(_) => todo!(),
-            ast::Stmt::Pass(_) => todo!(),
-            ast::Stmt::Break(_) => todo!(),
-            ast::Stmt::Continue(_) => todo!(),
-        }
-    }
+    let output = s_body(module.body);
+    println!("{}", output);
+}
 
+fn read_ast(source_path: &str, filepath: &str) -> Result<ast::Mod, rustpython_parser::ParseError> {
+    let source = std::fs::read_to_string(filepath).expect("read file");
+
+    rustpython_parser::parse(&source, Mode::Module, source_path)
 }
 
 fn s_functiondef(f: ast::StmtFunctionDef) -> String {
     // TODO decorators
-    let ast::StmtFunctionDef{
+    let ast::StmtFunctionDef {
         range,
-        name, args, body, decorator_list, returns, type_comment, type_params };
+        name,
+        args,
+        body,
+        decorator_list,
+        returns,
+        type_comment,
+        type_params,
+    };
 
     let mut out = String::with_capacity(256);
-    out.push("function ");
-    out.push(name.as_str());
-    out.push("(");
+    out.push_str("function ");
+    out.push_str(name.as_str());
+    out.push_str("(");
     // TODO are these the right arguments?
-    let args = args.posonlyargs.into_iter().map(|arg| {
-        // TODO use annotation
-        let output = arg.def.arg.as_str().to_owned();
-        if let Some(expr) = arg.default {
-            let expr = s_expr(expr);
-            output.push("=");
-            output.push_str(expr.as_str());
-        }
+    let args = args
+        .posonlyargs
+        .into_iter()
+        .map(|arg| {
+            // TODO use annotation
+            let mut output = arg.def.arg.as_str().to_owned();
+            if let Some(expr) = arg.default {
+                let expr = s_expr(expr);
+                output.push('=');
+                output.push_str(expr.as_str());
+            }
 
-        output
-    }).join(", ");
+            output
+        })
+        .join(",");
 
     out.push_str(&args);
 
-    let body = s_body(body);
+    out.push(')');
+    out.push('{');
 
-    out.push(&body);
+    let body = s_body(body);
+    out.push_str(&body);
+    out.push('}');
 
     out
 }
 
-fn s_body(body: Vec<ast::Stmt<_>>) -> String {
-    todo!()
+fn s_body(body: Vec<ast::Stmt<impl Any>>) -> String {
+    let elems = body.into_iter().map(s_stmt);
+
+    join(elems, "\n")
+}
+
+fn s_stmt(body: ast::Stmt<_>) -> String {
+    match body {
+        ast::Stmt::FunctionDef(stmt_function_def) => s_functiondef(stmt_function_def),
+        ast::Stmt::Expr(stmt_expr) => s_expr(stmt_expr),
+        ast::Stmt::AsyncFunctionDef(stmt_async_function_def) => todo(),
+        ast::Stmt::ClassDef(stmt_class_def) => todo(),
+        ast::Stmt::Return(stmt_return) => todo(),
+        ast::Stmt::Delete(stmt_delete) => todo(),
+        ast::Stmt::Assign(stmt_assign) => todo(),
+        ast::Stmt::TypeAlias(stmt_type_alias) => todo(),
+        ast::Stmt::AugAssign(stmt_aug_assign) => todo(),
+        ast::Stmt::AnnAssign(stmt_ann_assign) => todo(),
+        ast::Stmt::For(stmt_for) => todo(),
+        ast::Stmt::AsyncFor(stmt_async_for) => todo(),
+        ast::Stmt::While(stmt_while) => todo(),
+        ast::Stmt::If(stmt_if) => todo(),
+        ast::Stmt::With(stmt_with) => todo(),
+        ast::Stmt::AsyncWith(stmt_async_with) => todo(),
+        ast::Stmt::Match(stmt_match) => todo(),
+        ast::Stmt::Raise(stmt_raise) => todo(),
+        ast::Stmt::Try(stmt_try) => todo(),
+        ast::Stmt::TryStar(stmt_try_star) => todo(),
+        ast::Stmt::Assert(stmt_assert) => todo(),
+        ast::Stmt::Import(stmt_import) => todo(),
+        ast::Stmt::ImportFrom(stmt_import_from) => todo(),
+        ast::Stmt::Global(stmt_global) => todo(),
+        ast::Stmt::Nonlocal(stmt_nonlocal) => todo(),
+        ast::Stmt::Pass(stmt_pass) => todo(),
+        ast::Stmt::Break(stmt_break) => todo(),
+        ast::Stmt::Continue(stmt_continue) => todo(),
+    }
 }
 
 fn s_expr(expr: Box<ast::Expr<_>>) -> String {
-    todo!()
+    match expr {
+        ast::Expr::BoolOp(op) => {
+            let joiner = match op.op {
+                ast::BoolOp::And => "&&",
+                ast::BoolOp::Or => "||",
+            };
+
+            let x = op.values.into_iter().map(|expr| s_expr(expr));
+            join(x, joiner)
+        }
+        ast::Expr::NamedExpr(_) => todo(),
+        ast::Expr::BinOp(_) => todo(),
+        ast::Expr::UnaryOp(_) => todo(),
+        ast::Expr::Lambda(_) => todo(),
+        ast::Expr::IfExp(_) => todo(),
+        ast::Expr::Dict(_) => todo(),
+        ast::Expr::Set(_) => todo(),
+        ast::Expr::ListComp(_) => todo(),
+        ast::Expr::SetComp(_) => todo(),
+        ast::Expr::DictComp(_) => todo(),
+        ast::Expr::GeneratorExp(_) => todo(),
+        ast::Expr::Await(_) => todo(),
+        ast::Expr::Yield(_) => todo(),
+        ast::Expr::YieldFrom(_) => todo(),
+        ast::Expr::Compare(_) => todo(),
+        ast::Expr::Call(_) => todo(),
+        ast::Expr::FormattedValue(_) => todo(),
+        ast::Expr::JoinedStr(_) => todo(),
+        ast::Expr::Constant(_) => todo(),
+        ast::Expr::Attribute(_) => todo(),
+        ast::Expr::Subscript(_) => todo(),
+        ast::Expr::Starred(_) => todo(),
+        ast::Expr::Name(_) => todo(),
+        ast::Expr::List(_) => todo(),
+        ast::Expr::Tuple(_) => todo(),
+        ast::Expr::Slice(_) => todo(),
+    }
+}
+
+fn todo<T: Default>() -> T {
+    Default::default()
 }
